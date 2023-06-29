ba62bab2725ad6e7103e8e39d0f11e067fb1b9aa
torekleppe <tore.kleppe@uis.no>
2023-06-29 09:33:11 +0200

    pass include in testSystem

--- R/testSystem.R
+++ R/testSystem.R
@@ -3,12 +3,13 @@
 #' @description Build and run a simple model in order to test the installation/available C++ compiler
 #'
 
-testSystem<-function(){
+testSystem<-function(include=""){
   success <- FALSE
   mdl <- build(model.file=system.file('examples/testSystemModel.cpp',
                                       package = "pdmphmc"),
                model.class.name="model",
-               work.folder = paste0(getwd(),"/___testSystemWorkFolder___/"))
+               work.folder = paste0(getwd(),"/___testSystemWorkFolder___/"),
+               include=include)
   fit <- run(mdl,Tmax=1000.0)
   if(max(abs(fit@bin.eflag[[1]]))<0.1){
     message("model ran successfully")
